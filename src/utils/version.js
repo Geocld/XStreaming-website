f659e16b489aee0531ee35e9bedc9879861b6264
jiahao li <375822331@qq.com>
2025-01-22 18:54:10 +0800

    Update version.js

--- src/utils/version.js
+++ src/utils/version.js
@@ -1,7 +1,12 @@
 export default [
   {
-    version: "v1.3.2",
+    version: "v1.4.0",
     date: "2025-01-20",
+    description: "",
+  },
+  {
+    version: "v1.3.2",
+    date: "2025-01-22",
     description: "",
   },
 ];
